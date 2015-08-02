db89214bbe5256dc5cabab3b3d24c6c1ebb3afbe
Matthew Hasbach <hasbach.git@gmail.com>
2015-08-02 05:39:33 -0400

    Note durations are now passed as the third Tone.Note.route callback argument

--- lib/react-components/instruments/duration.jsx
+++ lib/react-components/instruments/duration.jsx
@@ -4,14 +4,14 @@ import common from '../../common';
 export default class extends React.Component {
     render() {
         return <select value={this.props.cursor.get()} onChange={common.changeSetting.bind(this)}>
-            <option value='1'>Whole Note</option>
-            <option value='2'>Half Note</option>
-            <option value='4'>Quarter Note</option>
-            <option value='8'>Eighth Note</option>
-            <option value='16'>Sixteenth Note</option>
-            <option value='32'>Thirty-Second Note</option>
-            <option value='64'>Sixty-Fourth Note</option>
-            <option value='128'>Hundred Twenty-Eighth Note</option>
+            <option value='1n'>Whole Note</option>
+            <option value='2n'>Half Note</option>
+            <option value='4n'>Quarter Note</option>
+            <option value='8n'>Eighth Note</option>
+            <option value='16n'>Sixteenth Note</option>
+            <option value='32n'>Thirty-Second Note</option>
+            <option value='64n'>Sixty-Fourth Note</option>
+            <option value='128n'>Hundred Twenty-Eighth Note</option>
         </select>
     }
 };
