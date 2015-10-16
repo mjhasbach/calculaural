d9a4d4a7214af78da465198ce1131a75e69323b7
Matthew Hasbach <hasbach.git@gmail.com>
2015-10-16 15:17:06 -0400

    Wait until the next digest cycle to instantiate the piano roll

--- ui/lib/directives/pianoRoll.js
+++ ui/lib/directives/pianoRoll.js
@@ -1,81 +1,83 @@
 import pixiPianoRoll from 'pixi-piano-roll';
 
-export default function() {
+export default function($timeout) {
     return {
         restrict: 'E',
         compile: function() {
             return {
                 post: function($scope, container) {
-                    let pianoRoll = pixiPianoRoll({
-                            renderer: 'CanvasRenderer',
-                            time: $scope.audio.playback.position,
-                            bpm: $scope.globalSetting.values.beatsPerMinute,
-                            pianoKeyWidth: container.width() / 8,
-                            width: container.width(),
-                            height: container.height(),
-                            noteFormat: 'Frequency',
-                            noteData: $scope.audio.instrument.notes.generate($scope.iterInstrument)
-                        }),
-                        $pianoRoll = $(pianoRoll.view);
+                    $timeout(function() {
+                        let pianoRoll = pixiPianoRoll({
+                                renderer: 'CanvasRenderer',
+                                time: $scope.audio.playback.position,
+                                bpm: $scope.globalSetting.values.beatsPerMinute,
+                                pianoKeyWidth: container.width() / 8,
+                                width: container.width(),
+                                height: container.height(),
+                                noteFormat: 'Frequency',
+                                noteData: $scope.audio.instrument.notes.generate($scope.iterInstrument)
+                            }),
+                            $pianoRoll = $(pianoRoll.view);
 
-                    container.append($pianoRoll);
+                        container.append($pianoRoll);
 
-                    $scope.iterInstrument.pianoRoll = pianoRoll;
+                        $scope.iterInstrument.pianoRoll = pianoRoll;
 
-                    $scope.$watch(
-                        function() {
-                            return [container[0].clientWidth, container[0].clientHeight].join('x');
-                        },
-                        function(size, oldSize) {
-                            if (size !== oldSize) {
-                                $pianoRoll.width(container.width());
-                                $pianoRoll.height(container.height());
+                        $scope.$watch(
+                            function() {
+                                return [container[0].clientWidth, container[0].clientHeight].join('x');
+                            },
+                            function(size, oldSize) {
+                                if (size !== oldSize) {
+                                    $pianoRoll.width(container.width());
+                                    $pianoRoll.height(container.height());
+                                }
                             }
-                        }
-                    );
+                        );
 
-                    $scope.$watch('globalSetting.values.pianoRollZoom', function(zoom, oldZoom) {
-                        if (zoom !== oldZoom) {
-                            pianoRoll.zoom = zoom;
-                        }
-                    });
+                        $scope.$watch('globalSetting.values.pianoRollZoom', function(zoom, oldZoom) {
+                            if (zoom !== oldZoom) {
+                                pianoRoll.zoom = zoom;
+                            }
+                        });
 
-                    $scope.$watch('globalSetting.values.pianoRollResolution', function(resolution, oldResolution) {
-                        if (resolution !== oldResolution) {
-                            pianoRoll.resolution = resolution;
-                        }
-                    });
+                        $scope.$watch('globalSetting.values.pianoRollResolution', function(resolution, oldResolution) {
+                            if (resolution !== oldResolution) {
+                                pianoRoll.resolution = resolution;
+                            }
+                        });
 
-                    $scope.$watch('globalSetting.values.beatsPerMinute', function(bpm, oldBPM) {
-                        if (bpm !== oldBPM) {
-                            pianoRoll.bpm = bpm;
-                        }
-                    });
+                        $scope.$watch('globalSetting.values.beatsPerMinute', function(bpm, oldBPM) {
+                            if (bpm !== oldBPM) {
+                                pianoRoll.bpm = bpm;
+                            }
+                        });
 
-                    $scope.$watchGroup([
-                        'globalSetting.values.transpose',
-                        'iterInstrument.transpose',
-                        'iterInstrument.numberQuantity',
-                        'iterInstrument.numberType',
-                        'iterInstrument.noteDuration',
-                        'iterInstrument.notesPerMeasure',
-                        'iterInstrument.tonic',
-                        'iterInstrument.scale',
-                        'iterInstrument.scaleBehavior',
-                        'iterInstrument.scaleDirection',
-                        'iterInstrument.chordType',
-                        'iterInstrument.arpeggiateChord',
-                        'iterInstrument.arpeggioDirection',
-                        'iterInstrument.arpeggioPeakAndValleyBehavior'
-                    ], function(oldData, newData) {
-                        if (oldData !== newData) {
-                            pianoRoll.noteData = $scope.audio.instrument.notes.generate($scope.iterInstrument);
-                        }
-                    });
+                        $scope.$watchGroup([
+                            'globalSetting.values.transpose',
+                            'iterInstrument.transpose',
+                            'iterInstrument.numberQuantity',
+                            'iterInstrument.numberType',
+                            'iterInstrument.noteDuration',
+                            'iterInstrument.notesPerMeasure',
+                            'iterInstrument.tonic',
+                            'iterInstrument.scale',
+                            'iterInstrument.scaleBehavior',
+                            'iterInstrument.scaleDirection',
+                            'iterInstrument.chordType',
+                            'iterInstrument.arpeggiateChord',
+                            'iterInstrument.arpeggioDirection',
+                            'iterInstrument.arpeggioPeakAndValleyBehavior'
+                        ], function(oldData, newData) {
+                            if (oldData !== newData) {
+                                pianoRoll.noteData = $scope.audio.instrument.notes.generate($scope.iterInstrument);
+                            }
+                        });
 
-                    if ($scope.state.playing) {
-                        pianoRoll.playback.play();
-                    }
+                        if ($scope.state.playing) {
+                            pianoRoll.playback.play();
+                        }
+                    }, 0);
                 }
             };
         }
